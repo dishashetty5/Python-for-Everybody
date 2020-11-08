86cc6a0bb9684d9c6c82ee20b4c61e5a84e9f590
disha shetty <50168017+dishashetty5@users.noreply.github.com>
2020-11-08 11:36:25 +0300

    Add files via upload

---
+++ chap 9 2.py
@@ -0,0 +1,8 @@
+filename=input("enter a file name: ")
+fhand=open(filename,'r')
+days={}
+for line in fhand:
+    if line.startswith("From "):
+        day=line.split()[2]
+        days[day]=days.get(day,0)+1
+print(days)
